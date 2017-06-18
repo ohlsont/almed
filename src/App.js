b055cf6a10becb05ad93956f34e262dd878a8356
Tomas Ohlson <ohlson.tomas@gmail.com>
2017-06-19 00:08:38 +0200

    getting stuff works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FlatButton, AppBar } from 'material-ui';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import GoogleMapReact from 'google-map-react';
 import { fitBounds } from 'google-map-react/utils';
+import himalaya from 'himalaya'
 import './App.css';
 
 // Needed for onTouchTap
@@ -18,30 +19,32 @@ type AlmPoint = {
     id: string,
 }
 
+const applyChildren = (json: Object, arr: Array<number>) => arr
+    .reduce((acc, item) => acc.children && acc.children[item] ? acc.children[item] : {}, json).content
 class App extends Component {
     state = {
         points: [],
         bounds: { nw: { lat: 0, lng: 0 }, se: { lat: 0, lng: 0 } },
     }
 
-    static async getAll(): Promise<any> {
-        const resp = await fetch('https://almedalsguiden.com/api?version=js', {
-            mode: 'cors',
-            method: 'post',
+    static async fetchJson(method: string, url: string, body: string) {
+        const resp = await fetch(url, {
+            mode: 'no-cors',
+            method,
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Type': 'text/plain',
                 'Access-Control-Allow-Origin': '*',
             },
-            body: 'search_place=a',
+            body,
         })
 
         switch (resp.status) {
-        case 204:
-            return {}
-        case 401:
-            throw new Error('bad permissions, 401 response code')
-        default:
-            break
+            case 204:
+                return {}
+            case 401:
+                throw new Error('bad permissions, 401 response code')
+            default:
+                break
         }
 
         if (!resp.ok) {
@@ -50,6 +53,82 @@ class App extends Component {
         }
 
         return resp.json() || {}
+    }
+    static async getIds() {
+        const url = 'https://almedalsguiden.com/main/search'
+        const resp = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: 'search=S%C3%B6k&' +
+            'freetext=&' +
+            'date_from=2017-07-02&' +
+            'date_to=2017-07-09&' +
+            'subject=&' +
+            'event_form=&' +
+            'eventType=&' +
+            'organizer=&' +
+            'orgtype=&' +
+            'place=&' +
+            'language=&' +
+            'status=&' +
+            'accessibility=',
+        })
+
+        switch (resp.status) {
+            case 204:
+                return {}
+            case 401:
+                throw new Error('bad permissions, 401 response code')
+            default:
+                break
+        }
+        const res = await resp.text()
+        const json = himalaya.parse(res)
+        const elements = json[2].children[3].children[5].children[5].children[1].children[3].children
+        return elements.map(elem => elem.attributes ? elem.attributes.href : null).filter(e => e)
+    }
+
+    static async getItem(href) {
+        const resp = await fetch(`https://almedalsguiden.com/${href}`)
+        const res = await resp.text()
+        const json = himalaya.parse(res)
+        const itemContent = json[2].children[3].children[5].children[5].children[3]
+
+        const p = itemContent.children[9].children
+        const a2 = Array.apply(null, {length: p.length}) // eslint-disable-line
+            .map(Number.call, Number).filter(no => no % 2 !== 0) // eslint-disable-line
+        const participants = a2.map(no => p[no].content)
+        return {
+            title: applyChildren(itemContent, [1,0]),
+            organiser: applyChildren(itemContent, [3,1,1]),
+            date: applyChildren(itemContent, [3,3,1]),
+            type: applyChildren(itemContent, [5,1,1]),
+            subject: applyChildren(itemContent, [5,3,1]),
+            language: applyChildren(itemContent, [5,5,1]),
+            location: applyChildren(itemContent, [5,7,1]),
+            locationDescription: applyChildren(itemContent, [5,9,1]),
+            description: applyChildren(itemContent, [7,0]),
+            participants,
+            green: applyChildren(itemContent, [11,1,1]),
+            availabilty: applyChildren(itemContent, [11,3,1]),
+            live: applyChildren(itemContent, [11,5,1]),
+            food: applyChildren(itemContent, [11,7,1]),
+            web: applyChildren(itemContent, [13,1,0,0]),
+            // contact: applyChildren(itemContent, [15,1,1,0,0]),
+            // contactOrg: applyChildren(itemContent, [15,1,1]),
+            // contactNumber: applyChildren(itemContent, [6,1,1]),
+        }
+    }
+
+    static async getAll(): Promise<any> {
+        return App.fetchJson(
+            'POST',
+            'https://almedalsguiden.com/api?version=js',
+            'search_place=a',
+        )
     }
 
     componentWillMount() {
@@ -73,9 +152,12 @@ class App extends Component {
     }
 
     async downloadSaveData(): Promise<void> {
-        const almData: { result: Array<AlmPoint> } = await App.getAll()
-        localStorage.setItem('items', JSON.stringify(almData.result))
-        this.setState({ points: almData.result })
+        // const almData: { result: Array<AlmPoint> } = await App.getAll()
+        // localStorage.setItem('items', JSON.stringify(almData.result))
+        // this.setState({ points: almData.result })
+        const ids = await App.getIds()
+        const allData = await Promise.all(ids.slice(0, 10).map(id => App.getItem(id)))
+        console.log('allData', allData)
     }
 
     render() {
@@ -90,8 +172,8 @@ class App extends Component {
                     title="Title"
                     iconElementRight={<FlatButton label={'Download'} onClick={() => this.downloadSaveData()} />}
                 />
-                <div style={{ height: '100vh' }}>
-                    {!!points.length && <GoogleMapReact
+                <div style={{ height: '90vh' }}>
+                    {false && !!points.length && <GoogleMapReact
                         defaultCenter={center}
                         defaultZoom={zoom}
                     >
