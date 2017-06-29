46409899fb279f2d3e163c72dad85ec7e6fb4aee
Tomas Ohlson <ohlson.tomas@gmail.com>
2017-06-29 23:39:12 +0200

    fix port issue

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 // @flow
 import app from './app'
 
-const { PORT = 8080 } = process.env
+const { PORT = 80 } = process.env
 // $FlowFixMe
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) // eslint-disable-line no-console
