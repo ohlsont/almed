4da56e3c81b2be996d31d0247eb43d888d65a75d
Tomas Ohlson <ohlson.tomas@gmail.com>
2018-06-24 22:52:55 +0200

    update build script

--- herokuDeploy.sh
+++ herokuDeploy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-git subtree push --prefix server heroku master
+#git subtree push --prefix server heroku master --force
+git push heroku `git subtree split --prefix server master`:master --force
