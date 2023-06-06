fc68dd50c85dfc284b982d94d4545675476f59e4
Tomas Ohlson <ohlson.tomas@gmail.com>
2023-06-06 22:45:00 +0200

    feat: extend event test suite
    Added more event tests and change accordingly.

---
+++ internal/server/debug.go
@@ -0,0 +1,68 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/ohlsont/almed/internal/event"
+)
+
+func addDebugHandles(ctx context.Context, client event.AlmedClient, mux *http.ServeMux) {
+	mux.HandleFunc("/ids", func(writer http.ResponseWriter, request *http.Request) {
+		ids, err := client.GetEventIds(ctx)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, err := writer.Write([]byte("bad id"))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			return
+		}
+		data, err := json.Marshal(ids)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		writer.Header().Set("Content-Type", "application/json")
+		if _, err = writer.Write(data); err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+	mux.HandleFunc("/event/", func(writer http.ResponseWriter, request *http.Request) {
+		parts := strings.Split(request.URL.Path, "/")
+		if len(parts) < 2 {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte("bad url"))
+			return
+		}
+		id, err := strconv.Atoi(parts[len(parts)-1])
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = writer.Write([]byte("bad id"))
+			return
+		}
+		ev, err := client.GetEvent(ctx, id)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			_, _ = writer.Write([]byte("bad event"))
+			fmt.Println(err)
+			return
+		}
+		data, err := json.Marshal(ev)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if _, err = writer.Write(data); err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
